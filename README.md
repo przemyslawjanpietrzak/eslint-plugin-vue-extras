f0096b49351a148407fc4ef3cb9329a3e43c23e7
Przemyslaw Jan Beigert <przemyslaw.jan.beigert@gmail.com>
2021-03-27 19:58:11 +0100

    find this usage in a beforeRouteEnter method

---
+++ README.md
@@ -0,0 +1,50 @@
+# eslint-plugin-vue-no-this-in-before-route-enter
+
+Don&#39;t use &#34;this&#34; i a beforeRouteEnter method
+
+## Installation
+
+You'll first need to install [ESLint](http://eslint.org):
+
+```
+$ npm i eslint --save-dev
+```
+
+Next, install `eslint-plugin-vue-no-this-in-before-route-enter`:
+
+```
+$ npm install eslint-plugin-vue-no-this-in-before-route-enter --save-dev
+```
+
+
+## Usage
+
+Add `vue-no-this-in-before-route-enter` to the plugins section of your `.eslintrc` configuration file. You can omit the `eslint-plugin-` prefix:
+
+```json
+{
+    "plugins": [
+        "vue-no-this-in-before-route-enter"
+    ]
+}
+```
+
+
+Then configure the rules you want to use under the rules section.
+
+```json
+{
+    "rules": {
+        "vue-no-this-in-before-route-enter/rule-name": 2
+    }
+}
+```
+
+## Supported Rules
+
+* Fill in provided rules here
+
+
+
+
+
---
+++ tests/lib/rules/no-this-in-before-route-enter.js
@@ -0,0 +1,90 @@
+/**
+ * @fileoverview  Don&#39;t use &#34;this&#34; i a beforeRouteEnter method
+ * @author Przemyslaw Jan Beigert
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require("../../../lib/rules/no-this-in-before-route-enter");
+var RuleTester = require("eslint").RuleTester;
+
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({
+    parser: require.resolve('vue-eslint-parser'),
+    parserOptions: { ecmaVersion: 2020, sourceType: 'module' }
+})
+ruleTester.run("no-this-in-before-route-enter", rule, {
+
+    valid: [
+
+        // give me some code that won't trigger a warning
+    ],
+
+    invalid: [
+        {
+            code: `
+<template>
+  <p>{{ greeting }} World!</p>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      greeting: "Hello"
+    };
+  },
+  beforeRouteEnter: function() {
+    this.xxx();
+  }
+};
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>`,
+            errors: [{
+                message: rule.errorMessage,
+            }]
+        },
+        {
+            code: `
+<template>
+  <p>{{ greeting }} World!</p>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      greeting: "Hello"
+    };
+  },
+  beforeRouteEnter() {
+    this.xxx();
+  }
+};
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>`,
+            errors: [{
+                message: rule.errorMessage,
+            }]
+        }
+    ]
+});
