cde50d1a08f70dae72b46dcd5a791f1082cf7787
Przemyslaw Jan Beigert <przemyslaw.jan.beigert@gmail.com>
2021-04-03 21:24:38 +0200

    add type-object-props rule

---
+++ tests/lib/rules/type-object-props.js
@@ -0,0 +1,81 @@
+/**
+ * @fileoverview force user to add type declaration to object props
+ * @author Przemyslaw Jan Beigert
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require("../../../lib/rules/type-object-props"),
+
+    RuleTester = require("eslint").RuleTester;
+
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const template = prop => `
+<script lang="ts">
+import { Prop } from 'vue/types/options';
+
+export default {
+	props: {
+		prop: {
+			${prop}
+		}
+	}
+}
+</script>  
+`;
+
+var ruleTester = new RuleTester({
+    parser: require.resolve('vue-eslint-parser'),
+    parserOptions: { ecmaVersion: 2020, sourceType: 'module', parser: '@typescript-eslint/parser', }
+});
+ruleTester.run("type-complex-props", rule, {
+
+    valid: [
+    	template('type: Object as Prop<{}>'),
+        template('type: String'),
+        template('type: Number'),
+        template('type: Boolean'),
+        template('type: [String, Number, Boolean]'),
+        template(''),
+    ],
+
+    invalid: [
+        {
+            code: template('type: Object'),
+            errors: [{
+                message: 'Object props has to be typed',
+            }]
+        },
+        {
+            code: template('type: Object as any'),
+            errors: [{
+                message: 'Object props should be typed like this: "type: Object as Prop<T>"',
+            }]
+        },
+        {
+        	code: template('type: Object as {}'),
+            errors: [{
+                message: 'Object props should be typed like this: "type: Object as Prop<T>"',
+            }]
+        },
+        {
+            code: template('type: Object as unknown'),
+            errors: [{
+                message: 'Object props should be typed like this: "type: Object as Prop<T>"',
+            }]
+        },
+        {
+            code: template('type: Object as string'),
+            errors: [{
+                message: 'Object props should be typed like this: "type: Object as Prop<T>"',
+            }]
+        }
+    ]
+});
