111ae6e6042001dda266340960c2c9771a332917
Przemyslaw Jan Beigert <przemyslaw.jan.beigert@gmail.com>
2021-05-10 21:52:21 +0200

    rule/add-no-empty-computed

---
+++ tests/lib/rules/no-empty-computed.js
@@ -0,0 +1,44 @@
+/**
+ * @fileoverview Prevent computed: {},
+ * @author Przemyslaw Jan Beigert
+ */
+"use strict";
+
+const rule = require("../../../lib/rules/no-empty-computed");
+const { RuleTester } = require("eslint");
+
+const template = (computed = '') => `
+<template>
+  <p>{{ greeting }} World!</p>
+</template>
+
+<script>
+export default {
+  ${computed}
+};
+</script>
+
+`
+
+const ruleTester = new RuleTester({
+    parser: require.resolve('vue-eslint-parser'),
+    parserOptions: { ecmaVersion: 2020, sourceType: 'module' }
+});
+
+ruleTester.run("no-empty-computed", rule, {
+
+    valid: [
+      template(),
+      template('computed: { method() {} }'),
+    ],
+
+    invalid: [
+        {
+            code: template('computed: {}'),
+            errors: [{
+                message: 'Remove empty computed object',
+            }],
+            output: template(),
+        }
+    ]
+});
